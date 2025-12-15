d7d2451befec53fbe01806780d9d6815d6e1e6f6
lock-desk <pintepinturasdeveloper@gmail>
2025-12-15 12:43:15 -0300

    chore: remove outdated backup and documentation files from candidate module
    - Deleted candidate/README.md containing outdated module documentation
    - Removed candidate/api_views.py.backup file (1482 lines)
    - Cleanup of unused backup and documentation files no longer needed

--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.views.static import serve
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from apps.candidate.auth_views import current_user
 from apps.candidate import views
@@ -57,6 +58,8 @@ urlpatterns = [
 # Serve media and static files
 # Note: In production, you should use a proper web server (Nginx/Apache) to serve these files
 # For now, we'll serve them through Django for simplicity
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
