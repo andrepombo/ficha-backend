e3995f1352b90035e9b15c461d249285f212da26
lock-desk <pintepinturasdeveloper@gmail>
2026-02-24 11:38:18 -0300

    Merge Migrations

---
+++ apps/candidate/migrations/0040_merge_0039.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('candidate', '0039_alter_candidate_worked_at_pinte_before'),
+        ('candidate', '0039_demovisitor_alter_candidate_worked_at_pinte_before'),
+    ]
+
+    operations = []
